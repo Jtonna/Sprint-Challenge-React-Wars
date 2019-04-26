0226b8b4324fe22ed627e6cbb02d77fa83dbac01
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-26 12:00:05 -0700

    Finished project
    Its ugly right now, im sorry

--- starwars/src/components/CardGenerator.js
+++ starwars/src/components/CardGenerator.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const CardGenerator = (props) => {
+	let theName = props.infobanana.name;
 	return(
 		<div className='info-card'>
-			<h3> {props.infobanana.name} </h3>
+			<h3> {theName} </h3>
 			<div className='unordered-list'>
 				<ul>
-					<li>Gender: {props.infobanana.gender}</li>
+					<li>It appears that {theName} gender is {props.infobanana.gender}</li>
+					<li></li>
+					<li><a href="{props.infobanana.homeworld}">Lets go on a trip to {props.infobanana.name}'s homeworld</a></li>
 				</ul>
 			</div>
 		</div>
